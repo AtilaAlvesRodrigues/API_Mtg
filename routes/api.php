ff362c80a7d499343eaee71337bfdf27d38f97cb
Átila Alves <atilalves48@gmail.com>
2025-02-08 12:51:26 -0300

    Modificado as Rotas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+use App\Http\Controllers\Api\MtgController;
+Route::get('/mtg/cards', [MtgController::class, 'getCards']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mtg_cards');
 });
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+//});
